4ab37cb8adc8dedb82e4342d7979b1da1f93a6cb
[bonface221] <[mainaboniface221@gmail.com]>
2022-04-10 23:41:46 +0300

    component:added the mock-quotes array

---
+++ src/app/mock-quotes.ts
@@ -0,0 +1,24 @@
+import { Quote } from "./Quote";
+
+export const QUOTES: Quote[] = [
+  {
+    id: 1,
+    quote:
+      '“Pay attention to your enemies,for they are the first to discover your mistakes"',
+    author: 'Antithenes',
+    publisher: 'kanyenje',
+    upvotes: 0,
+    downvotes: 0,
+    timeElapsed: new Date(),
+  },
+  {
+    id: 2,
+    quote:
+      '“Courage is not the strength to go on, it’s going on when you don’t have enough strength”',
+    author: 'Benjamin',
+    publisher: 'Peter',
+    upvotes: 0,
+    downvotes: 0,
+    timeElapsed: new Date(),
+  },
+];
