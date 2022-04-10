e2ba931388982288c5828ae6e4a627498467bdd0
[bonface221] <[mainaboniface221@gmail.com]>
2022-04-10 23:40:57 +0300

    component:added the form component

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import {faClock} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  faUser = faUser;
+  faQuote = faQuoteLeft;
+  faTime =faClock
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick() {
+    console.log(`hello there`)
+      
+    
+  }
+
+}
